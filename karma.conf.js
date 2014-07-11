7f6fa27293e247f977a88418e571caffd3ed8200
Tom Hamshere <thamshere@twitter.com>
2014-07-11 13:18:40 +0100

    Fix style issues
    * Don't upgrade version here - belongs in separate patch
    * Remove unnecessary props from karma.conf
    * Use Chrome as test browser
    * Fix indentation
    * Add karma-cli as dev dependency & remove from Travis setup

--- karma.conf.js
+++ karma.conf.js
@@ -39,53 +39,13 @@ module.exports = function (config) {
       'test/test-main.js',
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      //'Chrome',
-      //'ChromeCanary',
-      //'Firefox',
-      //'FirefoxAurora',
-      //'FirefoxNightly',
-      //'Opera',
-      'PhantomJS',
-      //'Safari'
+      'Chrome'
     ],
 
 
