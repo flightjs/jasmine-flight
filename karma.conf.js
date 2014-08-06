02e9632b4505fac228ccd805f1c4dd1509253792
Nicolas Gallagher <nicolasgallagher@gmail.com>
2014-08-06 13:33:10 -0700

    Update karma setup

--- karma.conf.js
+++ karma.conf.js
@@ -7,22 +7,18 @@ module.exports = function (config) {
   'use strict';
 
   config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
 
     // list of files / patterns to load in the browser
     files: [
-      // es5 shim for PhantomJS & IE8 & below
-      'bower_components/es5-shim/es5-shim.js',
-      'bower_components/es5-shim/es5-sham.js',
-      // hack to load RequireJS after the shim libs
-      'node_modules/requirejs/require.js',
-      'node_modules/karma-requirejs/lib/adapter.js',
       // jQuery
       'bower_components/jquery/dist/jquery.js',
       // jasmine-jquery
@@ -39,15 +35,13 @@ module.exports = function (config) {
       'test/test-main.js',
     ],
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-      'Chrome'
+    // frameworks to use
+    frameworks: [
+      'jasmine',
+      'requirejs'
     ],
 
+    reporters: [process.env.TRAVIS ? 'dots' : 'progress'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
